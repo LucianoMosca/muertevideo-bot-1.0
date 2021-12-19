69c959a122015e8c489ef823a11b78de3fd64abd
LucianoMosca <94749359+LucianoMosca@users.noreply.github.com>
2021-12-19 14:57:41 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="************",
+    client_secret="**************",
+    user_agent="<console:muertevideo:1.0>",
+    username="Muertevideo-bot",
+    password="*************",
+)
+
+sub = reddit.subreddit("sub-name")
+
+
+for submission in sub.new(limit=10):
+    print("-----------")
+    print(submission.title)
+    title_lower = submission.title.lower()
+    if "montevideo" in title_lower:
+        for comment in submission.comments:
+            
+            if hasattr(comment, 'body'):
+                comment_lower = comment.body.lower()
+                count = 0
+                if "muertevideo" in comment_lower:
+                    count += 1
+        if count == 0:
+            submission.reply("muertevideo")
